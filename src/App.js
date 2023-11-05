05ddd9e7cac79aa043a04895d1603f2cb68c2152
ImFaizanAhmed <imfaizanahmed6@gmail.com>
2023-11-06 01:31:37 +0500

    added logo

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import "./App.css";
 // import Plants from "./assets/plants.jpg";
 import Plants from "./assets/plant_plants.jpg";
 import GetSeeds from "./components/get-seed";
+import Logo from "./assets/logo.svg";
 
 function App() {
   return (
     <div className="App">
       <div className="header">
-        <div className="business-name">LocalSeedPro</div>
+        <div className="business-name"><img src={Logo} height={32} alt="logo"/></div>
         <div>
           <div>
             Login
@@ -27,7 +28,7 @@ function App() {
           Seed Schemes
         </div>
       </div>
-      <div className="desc" style={{background: "antiquewhite"}}>
+      <div className="desc" style={{background: "#a8d8b7"}}>
         <div className="sub-desc">
           Unlock the secrets of successful gardening with our app. Simply enter
           your location, and we'll recommend the perfect seeds for your soil and
